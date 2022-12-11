c3481ead163c8c99215706a4a18cae27cf84bb48
hugodms <hugo.domingues@epitech.eu>
2022-12-11 15:45:59 +0000

    Working on Contact

--- pages/Contact/index.tsx
+++ pages/Contact/index.tsx
@@ -4,6 +4,11 @@ import styles from "../../styles/Contact.module.css";
 import NavBar from "../../components/NavBar/NavBar";
 import Footer from "../../components/Footer/Footer";
 import Header from "../../components/Header/Header";
+import Button from "../../components/Button/Button";
+import LocaLogo from "../../public/location-pin.png";
+import PhoneCall from "../../public/phone-call.png";
+import Clock from "../../public/clock.png";
+import Image from "next/image";
 
 
 const Contact: NextPage = () => {
@@ -13,12 +18,26 @@ const Contact: NextPage = () => {
     <div className={styles.container}>
         <NavBar />
         <Header pageName="Contact"/>
+        <div className={styles.page_header}>
+            <h1>
+                Get In Touch With Us
+            </h1>
+            <p>
+                For More Information About Our Product & Services. Please Feel Free To Drop Us
+            </p>
+            <p>
+                An Email. Our Staff Always Be There To Help You Out. Do Not Hesitate!
+            </p>
+        </div>
         <div className={styles.parent}>
             <div className={styles.wide}>
                 <div className={styles.contactCard}>
-                    <h2 className={styles.titleCard}>
-                        Adress
-                    </h2>
+                    <div className={styles.header_text}>
+                        <Image className={styles.locationIcon} src={LocaLogo} height={36} width={36} />
+                        <h2 className={styles.titleCard}>
+                            Adress
+                        </h2>
+                    </div>
                     <p className={styles.textCard}>
                         236 5th SE Avenue,
                     </p>
@@ -30,9 +49,12 @@ const Contact: NextPage = () => {
                     </p>
                 </div>
                 <div className={styles.contactCard}>
-                    <h2 className={styles.titleCard}>
-                        Phone
-                    </h2>
+                    <div className={styles.header_text}>
+                        <Image className={styles.locationIcon} src={PhoneCall} height={36} width={36} />
+                        <h2 className={styles.titleCard}>
+                            Phone
+                        </h2>
+                    </div>
                     <p className={styles.textCard}>
                         Mobile: +(84) 546-6789
                     </p>
@@ -41,9 +63,12 @@ const Contact: NextPage = () => {
                     </p>
                 </div>
                 <div className={styles.contactCard}>
-                    <h2 className={styles.titleCard}>
-                        Working Time
-                    </h2>
+                    <div className={styles.header_text}>
+                        <Image className={styles.locationIcon} src={Clock} height={36} width={36} />
+                        <h2 className={styles.titleCard}>
+                            Working Time
+                        </h2>
+                    </div>
                     <p className={styles.textCard}>
                         Monday-Friday: 9:00 - 22:00
                     </p>
@@ -69,7 +94,8 @@ const Contact: NextPage = () => {
                     <p>Message</p>
                     <input className={styles.input_form} type="text" />
                 </div>
-                <input className={styles.submit} type="submit" value="Submit" />
+                <Button children="Submit" />
+                {/*<input className={styles.submit} type="submit" value="Submit" />*/}
             </div>
         </div>
         <Footer />
